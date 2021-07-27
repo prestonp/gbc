52aa2d8456ae98f0ef25fab42b7769e06fda9fa7
Preston Pham <prestonp08@gmail.com>
2021-07-27 01:24:59 +0100

    refactor: use embed for boot rom

--- cmd/gbc/main.go
+++ cmd/gbc/main.go
@@ -4,15 +4,19 @@ import (
 	"flag"
 	"log"
 
+	_ "embed"
+
 	"github.com/prestonp/gbc/pkg/gb"
 	"github.com/prestonp/gbc/pkg/gb/apu"
 	"github.com/prestonp/gbc/pkg/gb/gpu"
 )
 
+//go:embed boot.gb
+var boot []byte
+
 var (
 	debug = flag.Bool("debug", false, "debug mode")
 	file  = flag.String("f", "", "rom file")
-	boot  = flag.String("b", "bin/boot.gb", "path to boot rom")
 )
 
 func main() {
@@ -21,23 +25,14 @@ func main() {
 	if *file == "" {
 		log.Fatal("missing filename")
 	}
-	cartRom, err := gb.ReadRom(*file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if *boot == "" {
-		log.Fatal("missing boot rom")
-	}
-
-	bootRom, err := gb.ReadRom(*boot)
+	rom, err := gb.ReadRom(*file)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	gpu := gpu.New()
 	apu := apu.New()
-	mmu := gb.NewMMU(bootRom, cartRom, gpu, apu)
+	mmu := gb.NewMMU(boot, rom, gpu, apu)
 	cpu := gb.NewCPU(mmu, *debug)
 
 	cpu.Run()
